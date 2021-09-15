bd6a76485290a0a71f37d014d6ca6a1339f8ebdd
FrontRowWithj <adebusum@tcd.ie>
2021-09-15 13:47:19 +0100

    Added 'today' global date object

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import Header from "./Header";
 import "./css/app.css";
 import Timetable from "./Timetable";
+import { today } from "./util";
 
 const App = () => {
   const [next, setNext] = useState();
-  const day = new Date().getDay() - 1;
+  const day = today.getDay() - 1;
   const [curr, setCurr] = useState(day < 0 || day > 4 ? 0 : day);
   const [isTransitioning, setTransition] = useState(false);
   return (
