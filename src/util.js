32b81f668950682ee89cc9cafb4b644e907df704
FrontRowWithj <adebusum@tcd.ie>
2021-09-15 13:53:18 +0100

    Refactored handleTransitionEnd such that the scrollbar checking part is its own function to then be reused by other components. Added a global 'today' object and added some more utility functions

--- src/util.js
+++ src/util.js
@@ -1,6 +1,14 @@
 import { times } from "lodash";
 import { useEffect } from "react";
+
 const toggleClass = (elem) => elem.classList.toggle("slide-horizontally");
+export const setScrollBar = (elem) => {
+  const parent = elem.parentElement;
+  const { bottom } = elem.getBoundingClientRect();
+  const canScroll =
+    bottom > (window.innerHeight || document.documentElement.clientHeight);
+  parent.style.overflowY = canScroll ? "" : "hidden";
+};
 
 export const handleTransitionEnd = (
   elem,
@@ -16,11 +24,7 @@ export const handleTransitionEnd = (
     setCurr(i);
     setTransition(false);
     setAnimation(false);
-    const parent = elem.parentElement;
-    const { bottom } = elem.getBoundingClientRect();
-    const canScroll =
-      bottom > (window.innerHeight || document.documentElement.clientHeight);
-    parent.style.overflowY = canScroll ? "" : "hidden";
+    setScrollBar(elem);
     [...document.getElementsByClassName("timetable-page-container")].forEach(
       (elem, idx) => {
         elem.style.left = `${idx === i ? "" : "10"}0%`;
@@ -62,17 +66,19 @@ export const useAnimation = (
   ]);
 };
 
+export const today = new Date();
+
 const DAY_IN_MILLISECONDS = 86_400_000;
 export const getWeekDayDates = () => {
-  const today = new Date().getDay();
-  const offset = (1 - today) * DAY_IN_MILLISECONDS;
+  const currDay = today.getDay();
+  const offset = (1 - currDay) * DAY_IN_MILLISECONDS;
   const monday = Date.now() + offset;
   return times(5, (i) => new Date(monday + DAY_IN_MILLISECONDS * i)).map(
     getDateString
   );
 };
 
-const getDateString = (date = new Date()) => {
+const getDateString = (date) => {
   const month = [
     "January",
     "February",
@@ -89,3 +95,16 @@ const getDateString = (date = new Date()) => {
   ][date.getMonth()];
   return `${date.getDate()} ${month} ${date.getFullYear()}`;
 };
+
+const toNum = (s = "") => parseInt(s.substring(0, 2));
+const regex = /[0-9]+:/g;
+export const getTimeRange = (s) => s.match(regex).map(toNum);
+
+export const activityColors = [
+  { bgColor: "#2929A3", textColor: "white" },
+  { bgColor: "#E8AA14", textColor: "black" },
+  { bgColor: "#F5054F", textColor: "white" },
+  { bgColor: "#693696", textColor: "white" },
+];
+
+export const between = (x = 0, min = 0, max = 0) => x >= min && x <= max;
