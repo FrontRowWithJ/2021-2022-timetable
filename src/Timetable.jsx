6401fca13ca2481b776ca9b59a305d1416956844
FrontRowWithj <adebusum@tcd.ie>
2021-09-15 13:49:44 +0100

    Added behaviour such that the  current class is highlighted

--- src/Timetable.jsx
+++ src/Timetable.jsx
@@ -1,11 +1,14 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./css/timetable.css";
 import { times } from "lodash";
 import TimetablePage from "./TimetablePage";
 import timetableJSON from "./timetableData";
-import { useAnimation, getWeekDayDates } from "./util";
+import { useAnimation, getWeekDayDates, today } from "./util";
 
 const Timetable = ({ isTransitioning, setTransition, next, curr, setCurr }) => {
+  const HOUR_IN_MILLISECONDS = 1000 * 60 * 60;
+  const [hour, setHour] = useState(today.getHours());
+  const tillNextHour = 60 - today.getMinutes();
   const [isAnimating, setAnimation] = useState(false);
   const tableRefs = useRef(times(5, () => React.createRef()));
   const dates = getWeekDayDates();
@@ -18,6 +21,15 @@ const Timetable = ({ isTransitioning, setTransition, next, curr, setCurr }) => {
     tableRefs.current,
     setTransition
   );
+
+  useEffect(() => {
+    setTimeout(() => {
+      setInterval(() => {
+        setHour(today.getHours());
+      }, HOUR_IN_MILLISECONDS);
+    }, tillNextHour * 60 * 1000);
+  });
+  
   return (
     <div className="timetable-container">
       {Object.keys(timetableJSON).map((entry, i) => (
@@ -32,6 +44,7 @@ const Timetable = ({ isTransitioning, setTransition, next, curr, setCurr }) => {
           index={i}
           curr={curr}
           date={dates[i]}
+          hour={hour}
         />
       ))}
     </div>
