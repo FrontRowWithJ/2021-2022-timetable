451814c705077ff24092ac143297e0680b7f5ce2
FrontRowWithj <adebusum@tcd.ie>
2021-09-16 11:43:03 +0100

    Fixed issue where scroll bar wouldnt hide when it wasn't necessary to show

--- src/Timetable.jsx
+++ src/Timetable.jsx
@@ -8,20 +8,24 @@ import {
   today,
   HOUR_IN_MILLISECONDS,
   getLeft,
+  setScrollBar,
 } from "./util";
 
-const translate = (elem, dist, width) => {
-  if (elem) elem.style.left = dist + "px";
-};
+const translate = (e, d) => e && (e.style.left = d + "px");
+const isMouseEvent = (event) => /[Mm]ouse/i.test(event._reactName);
+const getEvent = (event) => (isMouseEvent(event) ? event : event.touches[0]);
 
 const Timetable = ({ isTransitioning, setTransition, next, curr, setCurr }) => {
+  const timetbaleRef = useRef(null);
   const [hour, setHour] = useState(today.getHours());
   const tillNextHour = 60 - today.getMinutes();
   const tableRefs = useRef(times(5, () => React.createRef()));
   const dates = getWeekDayDates();
   const [start, setStart] = useState(undefined);
   const [delta, setDelta] = useState(undefined);
+  const [isScrolling, setScrolling] = useState(undefined);
   useEffect(() => {
+    timetbaleRef.current.scrollTop = 0;
     tableRefs.current.forEach((ref, i) => {
       ref.current.style.left = getLeft(i, next);
     });
@@ -34,60 +38,75 @@ const Timetable = ({ isTransitioning, setTransition, next, curr, setCurr }) => {
     }, tillNextHour * 60 * 1000);
   }, [tillNextHour]);
 
-  const startSwipe = ({ pageX }) => {
-    setStart({ x: pageX, t: +new Date() });
+  useEffect(() => {
+    setScrollBar(tableRefs.current[curr].current);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const startSwipe = (event) => {
+    const { pageX, pageY } = getEvent(event);
+    setStart({ x: pageX, y: pageY, t: +new Date() });
   };
 
-  const moveSwipe = ({ pageX }) => {
-    const d = pageX - start.x;
+  const moveSwipe = (event) => {
+    if (!start) return;
+    if (!isMouseEvent(event))
+      if (event.touches.length > 1 || (event.scale && event.scale !== 1))
+        return;
+    const evt = getEvent(event);
+    const { pageX, pageY } = evt;
+    const d = { x: pageX - start.x, y: pageY - start.y };
     setDelta(d);
     const refs = tableRefs.current;
-    refs.forEach((ref) => (ref.current.style.transitionDuration = "0ms"));
     const l = curr ? refs[curr - 1].current : undefined;
     const m = refs[curr].current;
     const r = curr !== 4 ? refs[curr + 1].current : undefined;
-    const w = document.getElementsByClassName("timetable-container")[0]
-      .clientWidth;
-    translate(l, -w + d, w);
-    translate(m, d, w);
-    translate(r, w + d, w);
+    const w = timetbaleRef.current.clientWidth;
+    if (isScrolling === undefined)
+      setScrolling(!!(isScrolling || Math.abs(d.x) < Math.abs(d.y)));
+    if (!isScrolling) {
+      refs.forEach((ref) => (ref.current.style.transitionDuration = "0ms"));
+      [l, m, r].forEach((e, i) => translate(e, [-w, 0, w][i] + d.x));
+    }
   };
 
   const endSwipe = () => {
-    // tableRefs.current.forEach((ref) => toggle(ref.current));
-    tableRefs.current.forEach(
-      (ref) => (ref.current.style.transitionDuration = "")
-    );
-    const w = document.getElementsByClassName("timetable-container")[0]
-      .clientWidth;
+    const refs = tableRefs.current;
+    refs.forEach((ref) => (ref.current.style.transitionDuration = ""));
+    const w = timetbaleRef.current.clientWidth;
     const duration = +new Date() - start.t;
     const isValidSwipe =
-      (duration < 250 && delta > 20) || Math.abs(delta) > w / 2;
-    const refs = tableRefs.current;
+      (duration < 250 && delta.x > 20) || Math.abs(delta.x) > w / 2;
     const l = curr ? refs[curr - 1].current : undefined;
     const m = refs[curr].current;
     const r = curr !== 4 ? refs[curr + 1].current : undefined;
-    if (isValidSwipe) {
-      const direction = Math.abs(delta) / delta;
-      const pos =
-        direction < 0
-          ? [2 * -w, (curr !== 4) * -w, 0]
-          : [0, (curr !== 0) * w, 2 * w];
-      [l, m, r].forEach((elem, i) => translate(elem, pos[i], w));
-      if (direction < 0) setCurr(r ? curr + 1 : curr);
-      else setCurr(l ? curr - 1 : curr);
-    } else {
-      const pos = [-w, 0, w];
-      [l, m, r].forEach((elem, i) => translate(elem, pos[i], w));
+    if (!isScrolling) {
+      if (isValidSwipe) {
+        const direction = Math.abs(delta.x) / delta.x;
+        const pos =
+          direction < 0
+            ? [2 * -w, (curr !== 4) * -w, 0]
+            : [0, (curr !== 0) * w, 2 * w];
+        [l, m, r].forEach((elem, i) => translate(elem, pos[i]));
+        if (direction < 0) setCurr(r ? curr + 1 : curr);
+        else setCurr(l ? curr - 1 : curr);
+      } else {
+        const pos = [-w, 0, w];
+        [l, m, r].forEach((elem, i) => translate(elem, pos[i]));
+      }
     }
     setStart(undefined);
   };
   return (
     <div
+      ref={timetbaleRef}
       className="timetable-container"
       onMouseDown={(event) => startSwipe(event)}
-      onMouseMove={(event) => start && moveSwipe(event)}
+      onMouseMove={(event) => moveSwipe(event)}
       onMouseUp={() => endSwipe()}
+      onTouchStart={(event) => startSwipe(event)}
+      onTouchMove={(event) => moveSwipe(event)}
+      onTouchEnd={() => endSwipe()}
     >
       {Object.keys(timetableJSON).map((entry, i) => (
         <TimetablePage
