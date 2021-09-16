b697e311347b930dfc9b55e38ad5116c7c23fc7f
FrontRowWithj <adebusum@tcd.ie>
2021-09-16 01:05:16 +0100

    Removed custom useEffect, ontrantionEnd and toggle func. Added more utility functions.

--- src/util.js
+++ src/util.js
@@ -1,69 +1,11 @@
 import { times } from "lodash";
-import { useEffect } from "react";
 
-const toggleClass = (elem) => elem.classList.toggle("slide-horizontally");
 export const setScrollBar = (elem) => {
   const parent = elem.parentElement;
   const { bottom } = elem.getBoundingClientRect();
   const canScroll =
     bottom > (window.innerHeight || document.documentElement.clientHeight);
   parent.style.overflowY = canScroll ? "" : "hidden";
-};
-
-export const handleTransitionEnd = (
-  elem,
-  i,
-  curr,
-  isAnimating,
-  setCurr,
-  setTransition,
-  setAnimation
-) => {
-  toggleClass(elem);
-  if (i !== curr && isAnimating) {
-    setCurr(i);
-    setTransition(false);
-    setAnimation(false);
-    setScrollBar(elem);
-    [...document.getElementsByClassName("timetable-page-container")].forEach(
-      (elem, idx) => {
-        elem.style.left = `${idx === i ? "" : "10"}0%`;
-      }
-    );
-  }
-};
-
-export const useAnimation = (
-  isTransitioning,
-  isAnimating,
-  next,
-  curr,
-  setAnimation,
-  refs,
-  setTransition
-) => {
-  useEffect(() => {
-    if (isTransitioning && !isAnimating && next !== curr) {
-      setAnimation(true);
-      refs.forEach((ref, i) => {
-        ref.current.style.left = `${i === curr ? "" : "10"}0%`;
-      });
-      const _curr = refs[curr].current;
-      const _next = refs[next].current;
-      toggleClass(_curr);
-      toggleClass(_next);
-      _curr.style.left = "-100%";
-      _next.style.left = "0%";
-    } else if (isTransitioning && next === curr) setTransition(false);
-  }, [
-    curr,
-    next,
-    refs,
-    setAnimation,
-    isTransitioning,
-    isAnimating,
-    setTransition,
-  ]);
 };
 
 export const today = new Date();
@@ -107,4 +49,11 @@ export const activityColors = [
   { bgColor: "#693696", textColor: "white" },
 ];
 
-export const between = (x = 0, min = 0, max = 0) => x >= min && x <= max;
+export const between = (x = 0, min = 0, max = 0) => x >= min && x < max;
+
+export const HOUR_IN_MILLISECONDS = 1000 * 60 * 60;
+
+export const getLeft = (i, x) => (i - x) * 100 + "%";
+
+export const toggle = (elem) => elem.classList.toggle("slide-horizontally");
+
