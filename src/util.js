557c99662d660784ab8c4be1dddec5a7d00f6f8a
FrontRowWithj <adebusum@tcd.ie>
2021-09-21 12:48:29 +0100

    Removed exports that were used in a single file to the file they were used in. Moved getWeekDates to below getDateString to maintain lexical order.

--- src/util.js
+++ src/util.js
@@ -12,14 +12,6 @@ export const canScroll = (elem) => {
 };
 
 const DAY_IN_MILLISECONDS = 86_400_000;
-export const getWeekDayDates = () => {
-  const currDay = new Date().getDay();
-  const offset = (1 - currDay) * DAY_IN_MILLISECONDS;
-  const monday = Date.now() + offset;
-  return times(5, (i) => new Date(monday + DAY_IN_MILLISECONDS * i)).map(
-    getDateString
-  );
-};
 
 const getDateString = (date) => {
   const day = [
@@ -48,17 +40,13 @@ const getDateString = (date) => {
   return `${day} ${date.getDate()} ${month} ${date.getFullYear()}`;
 };
 
-const toNum = (s = "") => parseInt(s.substring(0, 2));
-const regex = /[0-9]+:/g;
-export const getTimeRange = (s) => s.match(regex).map(toNum);
-
-export const activityColors = [
-  { bgColor: "#2929A3", textColor: "white" },
-  { bgColor: "#E8AA14", textColor: "black" },
-  { bgColor: "#F5054F", textColor: "white" },
-  { bgColor: "#693696", textColor: "white" },
-];
-
-export const between = (x = 0, min = 0, max = 0) => x >= min && x < max;
+export const getWeekDayDates = () => {
+  const currDay = new Date().getDay();
+  const offset = (1 - currDay) * DAY_IN_MILLISECONDS;
+  const monday = Date.now() + offset;
+  return times(5, (i) => new Date(monday + DAY_IN_MILLISECONDS * i)).map(
+    getDateString
+  );
+};
 
 export const getLeft = (i, x) => (i - x) * 100 + "%";
