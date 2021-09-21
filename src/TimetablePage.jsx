42032ad406e2180c0e186c2b1461ecd91e731061
FrontRowWithj <adebusum@tcd.ie>
2021-09-21 12:46:36 +0100

    Transfered imported functions and constants from util.js to TimetablePage.jsx because they were only used here. Time values are now cached so they no longer need to be calculated. Increased opacity for inactive modules to increase the visibility of active modules. Made the elements non-draggable

--- src/TimetablePage.jsx
+++ src/TimetablePage.jsx
@@ -1,24 +1,21 @@
 import React, { useEffect, useCallback } from "react";
 import "./css/timetable-page.css";
 import { Timetable } from "./timetableData";
-import {
-  activityColors,
-  getTimeRange,
-  between,
-  setScrollBar,
-  getLeft,
-} from "./util";
+import { setScrollBar, getLeft } from "./util";
 
+const activityColors = [
+  { bgColor: "#2929A3", textColor: "white" },
+  { bgColor: "#E8AA14", textColor: "black" },
+  { bgColor: "#F5054F", textColor: "white" },
+  { bgColor: "#693696", textColor: "white" },
+];
+const between = (x = 0, min = 0, max = 0) => x >= min && x < max;
+const toNum = (s) => parseInt(s.substring(0, 2));
+const getTimeRange = (s) => s.match(/[0-9]+:/g).map(toNum);
 const TimetablePage = (props) => {
   const { refs, curr, index, date, hour, schedule, tableRef } = props;
   const currDay = new Date().getDay() - 1;
-  const classes = schedule
-    .map((cell, i) => {
-      if (cell)
-        cell.time = `${("" + (9 + i)).padStart(2, "0")}:00 - ${10 + i}:00`;
-      return cell;
-    })
-    .filter((cell) => cell !== undefined);
+  const classes = schedule.filter((cell) => cell !== undefined);
   const handleResize = useCallback(
     () => setScrollBar(refs[curr].current),
     [curr, refs]
@@ -32,22 +29,27 @@ const TimetablePage = (props) => {
       className="timetable-page-container"
       ref={tableRef}
       style={{ left: getLeft(index, curr) }}
+      draggable={false}
     >
-      <div className="date">{date}</div>
+      <div draggable={false} className="date">
+        {date}
+      </div>
       {classes.map((lesson, i) => {
         const { bgColor, textColor } = activityColors[lesson.activity];
         const { module, isOnline, activity, time, classroom } = lesson;
         const [start, end] = getTimeRange(time);
         const opacity =
-          between(hour, start, end) && index === currDay ? 1 : 0.5;
+          between(hour, start, end) && index === currDay ? 1 : 0.3;
         return (
           <div
             key={i}
             className="cell"
+            draggable={false}
             style={{
               boxShadow: "0px 7px 13px -7px " + bgColor,
               color: textColor,
               opacity: opacity,
+              height: `${(end - start) * 6}rem`,
             }}
           >
             <div
@@ -62,11 +64,15 @@ const TimetablePage = (props) => {
               className="bottom-1"
               style={{ backgroundColor: bgColor }}
             ></div>
-            <div className="bg" style={{ backgroundColor: bgColor }}>
-              <div>{module}</div>
-              <div>Classroom: {classroom}</div>
-              <div>{time}</div>
-              <div>{Timetable.ACTIVITIES[activity]}</div>
+            <div
+              draggable={false}
+              className="bg"
+              style={{ backgroundColor: bgColor }}
+            >
+              <div draggable={false}>{module}</div>
+              <div draggable={false}>Classroom: {classroom}</div>
+              <div draggable={false}>{time}</div>
+              <div draggable={false}>{Timetable.ACTIVITIES[activity]}</div>
             </div>
           </div>
         );
