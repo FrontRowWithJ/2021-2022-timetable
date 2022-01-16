7b980f31ff6ef3be3f9cf1056ccc93104c83adcf
FrontRowWithJ <adebusum@tcd.ie>
2022-01-16 19:38:19 +0000

    Added menu item component that goes into the menu

---
+++ src/MenuItem.jsx
@@ -0,0 +1,16 @@
+import "./css/menu-item.css";
+
+const MenuItem = ({ text, onclick }) => {
+  return (
+    <div className="menu-item">
+      <button type="button" onClick={onclick}>
+        {text}
+      </button>
+      <div className="bg-div" style={{ backgroundColor: "#585ce4" }}></div>
+      <div className="bg-div" style={{ backgroundColor: "#585ce4" }}></div>
+    </div>
+  );
+};
+
+export default MenuItem;
+
