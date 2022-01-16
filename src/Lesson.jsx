333d170e8bed373c8a6c99b854be080264e5f3ad
FrontRowWithJ <adebusum@tcd.ie>
2022-01-16 19:06:30 +0000

    Separated the Lesson part of the timetable to its own campaign

---
+++ src/Lesson.jsx
@@ -0,0 +1,50 @@
+import { Timetable } from "./timetableData";
+
+const between = (x = 0, min = 0, max = 0) => x >= min && x < max;
+const toNum = (s) => parseInt(s.substring(0, 2));
+const getTimeRange = (s) => s.match(/[0-9]+:/g).map(toNum);
+
+const activityColors = [
+  { bgColor: "#2929A3", textColor: "white" },
+  { bgColor: "#E8AA14", textColor: "black" },
+  { bgColor: "#F5054F", textColor: "white" },
+  { bgColor: "#693696", textColor: "white" },
+];
+
+const Lesson = ({ lesson, hour, index, currDay }) => {
+  const { bgColor, textColor } = activityColors[lesson.activity];
+  const { module, isOnline, activity, time, classroom } = lesson;
+  const [start, end] = getTimeRange(time);
+  const opacity = between(hour, start, end) && index === currDay ? 1 : 0.3;
+  return (
+    <div
+      className="cell"
+      draggable={false}
+      style={{
+        boxShadow: "0px 7px 13px -7px " + bgColor,
+        color: textColor,
+        opacity: opacity,
+        height: `${(end - start) * 6}rem`,
+      }}
+    >
+      <div
+        className="online-indicator"
+        style={{ background: isOnline ? "#00FF00" : "red" }}
+      ></div>
+      <div className="bottom-0" style={{ backgroundColor: bgColor }}></div>
+      <div className="bottom-1" style={{ backgroundColor: bgColor }}></div>
+      <div
+        draggable={false}
+        className="bg"
+        style={{ backgroundColor: bgColor }}
+      >
+        <div draggable={false}>{module}</div>
+        <div draggable={false}>Classroom: {classroom}</div>
+        <div draggable={false}>{time}</div>
+        <div draggable={false}>{Timetable.ACTIVITIES[activity]}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Lesson;
--- src/TimetablePage.jsx
+++ src/TimetablePage.jsx
@@ -1,17 +1,8 @@
 import React, { useEffect, useCallback } from "react";
 import "./css/timetable-page.css";
-import { Timetable } from "./timetableData";
 import { setScrollBar, getLeft } from "./util";
+import Lesson from "./Lesson";
 
-const activityColors = [
-  { bgColor: "#2929A3", textColor: "white" },
-  { bgColor: "#E8AA14", textColor: "black" },
-  { bgColor: "#F5054F", textColor: "white" },
-  { bgColor: "#693696", textColor: "white" },
-];
-const between = (x = 0, min = 0, max = 0) => x >= min && x < max;
-const toNum = (s) => parseInt(s.substring(0, 2));
-const getTimeRange = (s) => s.match(/[0-9]+:/g).map(toNum);
 const TimetablePage = (props) => {
   const { refs, curr, index, date, hour, schedule, tableRef } = props;
   const currDay = new Date().getDay() - 1;
@@ -34,46 +25,29 @@ const TimetablePage = (props) => {
       <div draggable={false} className="date">
         {date}
       </div>
-      {classes.map((lesson, i) => {
-        const { bgColor, textColor } = activityColors[lesson.activity];
-        const { module, isOnline, activity, time, classroom } = lesson;
-        const [start, end] = getTimeRange(time);
-        const opacity =
-          between(hour, start, end) && index === currDay ? 1 : 0.3;
+      {classes.map((lessons, i) => {
         return (
-          <div
-            key={i}
-            className="cell"
-            draggable={false}
-            style={{
-              boxShadow: "0px 7px 13px -7px " + bgColor,
-              color: textColor,
-              opacity: opacity,
-              height: `${(end - start) * 6}rem`,
-            }}
-          >
-            <div
-              className="online-indicator"
-              style={{ background: isOnline ? "#00FF00" : "red" }}
-            ></div>
-            <div
-              className="bottom-0"
-              style={{ backgroundColor: bgColor }}
-            ></div>
-            <div
-              className="bottom-1"
-              style={{ backgroundColor: bgColor }}
-            ></div>
-            <div
-              draggable={false}
-              className="bg"
-              style={{ backgroundColor: bgColor }}
-            >
-              <div draggable={false}>{module}</div>
-              <div draggable={false}>Classroom: {classroom}</div>
-              <div draggable={false}>{time}</div>
-              <div draggable={false}>{Timetable.ACTIVITIES[activity]}</div>
-            </div>
+          <div key={i} draggable={false} className="test">
+            {Array.isArray(lessons) ? (
+              lessons.map((lesson, i) => {
+                return (
+                  <Lesson
+                    key={`${lesson.time}_${lesson.moduleCode}_${i}`}
+                    lesson={lesson}
+                    hour={hour}
+                    index={index}
+                    currDay={currDay}
+                  />
+                );
+              })
+            ) : (
+              <Lesson
+                lesson={lessons}
+                hour={hour}
+                index={index}
+                currDay={currDay}
+              />
+            )}
           </div>
         );
       })}
