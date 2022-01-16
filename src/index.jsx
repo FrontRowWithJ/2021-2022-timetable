c6734b8e895b8c6f0e6322c8d177aaeac1dbc3d4
FrontRowWithJ <adebusum@tcd.ie>
2022-01-16 18:34:36 +0000

    Added caching strategy

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { registerRoute } from "workbox-routing";
+import * as strategies from "workbox-strategies";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+serviceWorkerRegistration.register();
+
+registerRoute(
+  "https://cdn.jsdelivr.net/npm/lzutf8/build/production/lzutf8.min.js",
+  new strategies.CacheFirst()
+);
+
+registerRoute(
+  "https://db.onlinewebfonts.com/c/860c3ec7bbc5da3e97233ccecafe512e?family=Circular Std Book",
+  new strategies.CacheFirst()
+);
+
+registerRoute(
+  "https://db.onlinewebfonts.com/t/860c3ec7bbc5da3e97233ccecafe512e.woff2",
+  new strategies.CacheFirst()
+);
+
+registerRoute(
+  "https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap",
+  new strategies.CacheFirst()
+);
+
 
 reportWebVitals();
