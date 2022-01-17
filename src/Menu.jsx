1c24ad47a211f358d46f16ab677bb667200344aa
FrontRowWithJ <adebusum@tcd.ie>
2022-01-17 23:53:16 +0000

    Refactored Menu.jsx to take advantation of better component composition to avoid prop drilling

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,13 +1,10 @@
 import React, { useRef } from "react";
 import "./css/kebab.css";
-import MenuItem from "./MenuItem";
-import { compressTimetable } from "./util";
 
-const Kebab = ({ timetableJSON }) => {
+const Kebab = ({ children }) => {
   const list = ["first", "middle", "last"];
   const kebabRef = list.map(() => React.createRef(null));
   const menuRef = useRef(null);
-
   return (
     <>
       <div
@@ -26,19 +23,7 @@ const Kebab = ({ timetableJSON }) => {
         <div ref={kebabRef[2]} className="circle anim-last-backward"></div>
       </div>
       <div ref={menuRef} className="menu anim-menu-backward">
-        <div className="menu-item-container">
-          <MenuItem
-            text={"Generate URL"}
-            onclick={() => {
-              const base64 = compressTimetable(timetableJSON, "Base64");
-              const url = `https://www.timetable.com?timetable=${base64}`;
-              console.log(url);
-            }}
-          />
-          <MenuItem text={"Customize"} onclick={() => {}} />
-          <MenuItem text={"Enable Notifications"} onclick={() => {}} />
-          <MenuItem text={"Reset Timetable"} onclick={() => {}} />
-        </div>
+        <div className="menu-item-container">{children}</div>
       </div>
     </>
   );
