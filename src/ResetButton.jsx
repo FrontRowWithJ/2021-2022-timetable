87e58c0c65cfc990358037f648d2f4ac0aeb171e
FrontRowWithJ <adebusum@tcd.ie>
2022-01-17 23:58:39 +0000

    Added reset button component that removes the timetables cached data

---
+++ src/ResetButton.jsx
@@ -0,0 +1,12 @@
+import "./css/reset-button.css";
+import ResetIcon from "./resources/reset.svg";
+
+const ResetButton = ({ id, resetTimetable }) => {
+  return (
+    <div className="reset-button" id={id} onClick={() => resetTimetable()}>
+      <img alt="reset-button" src={ResetIcon}/>
+    </div>
+  );
+};
+
+export default ResetButton;
