359b12824ea4b074559a63a985e5a44d281604be
FrontRowWithJ <adebusum@tcd.ie>
2022-01-17 23:54:13 +0000

    Added component that acts as the background for the x button component

---
+++ src/XButtonBGSVG.jsx
@@ -0,0 +1,19 @@
+const XButtonBG = ({ className }) => {
+  return (
+    <svg
+      className={className}
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      viewBox="0 0 1000 500"
+      enableBackground="new 0 0 1000 500"
+      xmlSpace="preserve"
+    >
+      <path d="M 0 500 A 166.67 166.67 0 0 0 166.67 333.33000000000004 A 333.33 333.33 0 1 1 833.3299999999999 333.33000000000004 A 166.67 166.67 0 0 0 1000 500 Z" />
+    </svg>
+  );
+};
+
+export default XButtonBG;
