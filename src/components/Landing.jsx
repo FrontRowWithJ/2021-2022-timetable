0695cc23863668392da92e61e2e903df3f3557f7
FrontRowWithJ <adebusum@tcd.ie>
2022-01-19 05:46:40 +0000

    Refactored Landing.jsx to use a separate component for each of the tutorials

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from "react";
 import "../css/landing.css";
 import { compressTimetable, generateTimetableJSON } from "../util";
+import FireFoxTutorial from "./FirefoxTutorial";
+import TutorialSelector from "./TutorialSelector";
 
 const Landing = ({ enableTimetable, setTimetable }) => {
   const [hasText, setText] = useState(false);
@@ -28,7 +30,10 @@ const Landing = ({ enableTimetable, setTimetable }) => {
                 onClick={() => {
                   const { value } = textAreaRef.current;
                   const timetable = generateTimetableJSON(value);
-                  const compressed = compressTimetable(timetable, "StorageBinaryString");
+                  const compressed = compressTimetable(
+                    timetable,
+                    "StorageBinaryString"
+                  );
                   window.localStorage.setItem("timetable", compressed);
                   setTimetable(timetable);
                   enableTimetable(true);
@@ -39,46 +44,8 @@ const Landing = ({ enableTimetable, setTimetable }) => {
             </div>
           </div>
         </div>
-        <div className="tutorial-container">
-          <ul>
-            <li>
-              Login in to
-              <a href="https://my.tcd.ie" target="_blank" rel="noreferrer">
-                &nbsp;my.tcd.ie
-              </a>
-            </li>
-            <li>Select the plus sign and navigate to My Timetable.</li>
-            <li>
-              In the My Timetable section, select "View my Own Student
-              Timetable".
-            </li>
-            <li>Right Click and select inspect.</li>
-            <li>
-              A panel should have appeared on the side of your screen showing
-              some fancy looking text. It's actually the this webpage but in
-              html.
-            </li>
-            <li>
-              At the top of the panel you should see some text that says{" "}
-              <i style={{ color: "#56565A" }}>&lt;!DOCTYPE html&gt;</i>.
-              <br />
-              Right click on the text underneath that says &lt;
-              <span style={{ color: "#0074E8" }}>html&nbsp;</span>
-              <span style={{ color: "#DD00A9" }}>class</span>
-              <span>="</span>
-              <span style={{ color: "#003EAA" }}>tablesaw-enhanced</span>
-              <span>"...</span>
-            </li>
-            <li>Hover over Copy in the context menu and select Inner HTML.</li>
-            <li>
-              Return to this website and paste the text into the textbox above.
-            </li>
-            <li>
-              Click the "Generate timetable" button and enjoy your new found
-              timetable!
-            </li>
-          </ul>
-        </div>
+        <TutorialSelector />
+        <FireFoxTutorial />
       </div>
     </main>
   );
