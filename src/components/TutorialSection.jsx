4afb9456cc4675f96b58c4605f04450509f6478d
FrontRowWithJ <adebusum@tcd.ie>
2022-01-20 17:19:29 +0000

    Added functionality that highlights the step that you click on.

--- src/components/TutorialSection.jsx
+++ src/components/TutorialSection.jsx
@@ -1,16 +1,54 @@
+import { useEffect, useRef, useState } from "react";
 import "../css/tutorial-section.css";
 
 const TutorialSection = ({ alt, src, children, noImage, step, style }) => {
+  const [isOverlayVisible, setOverlayVisibility] = useState(false);
+  const [opacity, setOpacity] = useState(0);
+  const overlayRef = useRef(null);
+  useEffect(() => {
+    if (overlayRef.current) {
+      const { current: div } = overlayRef;
+      div.style.opacity = opacity;
+    }
+  });
   return (
-    <div className="tutorial-section-container">
+    <div
+      className="tutorial-section-container"
+      style={{ cursor: noImage ? "default" : "pointer" }}
+      onClick={() =>
+        !isOverlayVisible &&
+        !opacity &&
+        (setOpacity(() => 1) || setOverlayVisibility(true))
+      }
+    >
+      {!noImage && isOverlayVisible && (
+        <div
+          className="tutorial-overlay"
+          ref={overlayRef}
+          onClick={() =>
+            isOverlayVisible &&
+            opacity &&
+            (setOpacity(() => 0) ||
+              setTimeout(() => setOverlayVisibility(false), 600))
+          }
+        >
+          <div>
+            <img alt={alt} src={src}></img>
+            <div className="tutorial-text-container" style={style ? style : {}}>
+              <div className="step-no">{step}</div>
+              <div className="step">{children}</div>
+            </div>
+          </div>
+        </div>
+      )}
       {!noImage && (
         <div className="tutorial-img-container">
           <img alt={alt} src={src} />
         </div>
       )}
       <div className="tutorial-text-container" style={style ? style : {}}>
-        <div className="step">{step}</div>
-        {children}
+        {!noImage && <div className="step-no">{step}</div>}
+        <div className="step">{children}</div>
       </div>
     </div>
   );
