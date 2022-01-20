d5148391eae11010e8f35cd7cd843aca2c4f6006
FrontRowWithJ <adebusum@tcd.ie>
2022-01-20 17:18:15 +0000

    Improved textarea design

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,19 +3,39 @@ import "../css/landing.css";
 import { compressTimetable, generateTimetableJSON } from "../util";
 import Tutorial from "./Tutorial";
 import TutorialSelector from "./TutorialSelector";
+import Error from "./Error";
 
 const Landing = ({ enableTimetable, setTimetable }) => {
   const [hasText, setText] = useState(false);
   const textAreaRef = useRef(null);
   const [clickedBrowser, setClickedBrowser] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
+  const errorRef = useRef(null);
+  const generateTimetable = () => {
+    const { value } = textAreaRef.current;
+    try {
+      const timetable = generateTimetableJSON(value);
+      const compressed = compressTimetable(timetable, "StorageBinaryString");
+      window.localStorage.setItem("timetable", compressed);
+      setTimetable(timetable);
+      enableTimetable(true);
+    } catch (error) {
+      if (value.length === 0) setErrorMessage("Textbox is empty.");
+      else setErrorMessage("HTML is incorrect.");
+      const { current } = errorRef;
+      current.style.top = "0";
+      setTimeout(() => (current.style.top = ""), 2000);
+    }
+  };
   return (
     <main>
+      <Error errorMessage={errorMessage} errorRef={errorRef} />
       <div className="landing-container">
         <div id="dummy"></div>
         <div className="input-container">
           <div id="wrapper">
             <div id="text-container">
-              <textarea
+              <textarea spellCheck="false"
                 ref={textAreaRef}
                 onChange={({ target }) => {
                   setText(target.value.length !== 0);
@@ -27,21 +47,14 @@ const Landing = ({ enableTimetable, setTimetable }) => {
             </div>
             <div id="button-container">
               <button
+                id="timetable-button"
                 type="button"
-                onClick={() => {
-                  const { value } = textAreaRef.current;
-                  const timetable = generateTimetableJSON(value);
-                  const compressed = compressTimetable(
-                    timetable,
-                    "StorageBinaryString"
-                  );
-                  window.localStorage.setItem("timetable", compressed);
-                  setTimetable(timetable);
-                  enableTimetable(true);
-                }}
+                onClick={generateTimetable}
               >
                 Generate Timetable
               </button>
+              <div></div>
+              <div></div>
             </div>
           </div>
         </div>
