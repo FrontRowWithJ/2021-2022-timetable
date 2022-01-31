992f104b85b84310f8cc43218fb9bf61b82cc0dc
FrontRowWithJ <adebusum@tcd.ie>
2022-01-31 15:42:31 +0000

    Minor Changes
    Reorganised functions based on their internal dependancy. Removed posToColor. renamed the variables in default settings to make more sense and now included txtColor. color conversion functions now input and output normalised hsl and rgb values.

--- src/misc/colourFunctions.js
+++ src/misc/colourFunctions.js
@@ -1,46 +1,21 @@
-import { times } from "./getWeekdayDates.js";
-
-const getLuminosity = (color = "#000000") => {
-  const [rL, gL, bL] = color
-    .substring(1)
-    .match(/.{1,2}/g)
-    .map((str) => parseInt(str, 16))
-    .map((hex) => {
-      let c = hex / 255.0;
-      return c <= 0.03928 ? c / 12.92 : Math.pow((c + 0.055) / 1.055, 2.4);
-    });
-  return 0.2126 * rL + 0.7152 * gL + 0.0722 * bL;
-};
-
-export const getTextColor = (bgColor) =>
-  getLuminosity(bgColor) > 0.179 ? "#000000" : "#FFFFFF";
-export const activityColors = ["#2929A3", "#E8AA14", "#F5054F", "#693696"];
-export const textColors = activityColors.map(getTextColor);
-
-const hex2rgb = (hex = "") =>
+export const hex2rgb = (hex = "") =>
   hex
     .substring(1)
     .match(/.{1,2}/g)
     .map((str) => parseInt(str, 16) / 255);
 
-const { max, min } = Math;
-
-// input: h as an angle in [0,360] and s,l in [0,1] - output: r,g,b in [0,1]
-export const hsl2rgb = (h, s, l) => {
-  const a = s * min(l, 1 - l);
-  const f = (n, k = (n + h / 30) % 12) => l - a * max(min(k - 3, 9 - k, 1), -1);
-  return (
-    "#" +
-    [0, 8, 4]
-      .map((n) => ((f(n) * 255) | 0).toString(16).padStart(2, "0"))
-      .join("")
+const getLuminosity = (r, g, b) => {
+  const [rl, gl, bl] = [r, g, b].map((c) =>
+    c <= 0.03928 ? c / 12.92 : Math.pow((c + 0.055) / 1.055, 2.4)
   );
+  return 0.2126 * rl + 0.7152 * gl + 0.0722 * bl;
 };
 
+const { max, min } = Math;
 /**
  * Converts an RGB color value to HSL. Conversion formula
  * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
- * Assumes r, g, and b are contained in the set [0, 255] and
+ * Assumes r, g, and b are contained in the set [0, 1] and
  * returns h, s, and l in the set [0, 1].
  *
  * @param   {number}  r       The red color value
@@ -48,13 +23,12 @@ export const hsl2rgb = (h, s, l) => {
  * @param   {number}  b       The blue color value
  * @return  {Array}           The HSL representation
  */
-const rgb2hsl = (r, g, b) => {
+const rgb2hsl = (r = 0, g = 0, b = 0) => {
   const max = Math.max(r, g, b);
   const min = Math.min(r, g, b);
   let h,
     s,
     l = (max + min) / 2;
-
   if (max === min) {
     h = s = 0; // achromatic
   } else {
@@ -76,34 +50,45 @@ const rgb2hsl = (r, g, b) => {
   return [h, s, l];
 };
 
-export const colorLuminance = (hex, lum = 0) => {
-  // convert to decimal and change luminosity
+export const getTextColor = (r, g, b) =>
+  getLuminosity(r, g, b) > 0.179 ? "#000000" : "#FFFFFF";
+
+export const activityColors = ["#2929A3", "#E8AA14", "#F5054F", "#693696"];
+
+export const textColors = activityColors.map((color) =>
+  getTextColor(hex2rgb(color))
+);
+
+// input: h,s,l in [0,1] - output: r,g,b in [0,1]
+export const hsl2rgb = (h = 0, s = 1, l = 0.5) => {
+  h = (h * 360) | 0;
+  const a = s * min(l, 1 - l);
+  const f = (n, k = (n + h / 30) % 12) => l - a * max(min(k - 3, 9 - k, 1), -1);
   return (
     "#" +
-    times(3, (i) => {
-      let c = parseInt(hex.substr(1 + i * 2, 2), 16);
-      c = (Math.min(Math.max(0, c + c * lum), 255) | 0).toString(16);
-      return c.padStart(2, "0");
-    }).join("")
+    [0, 8, 4]
+      .map((n) => ((f(n) * 255) | 0).toString(16).padStart(2, "0"))
+      .join("")
   );
 };
 
-export const posToColor = (x, y, width, height, hex) => {
-  const s = x / width;
-  const l = (1 - (x / width) * 0.5) * (1 - y / height);
-  const [r, g, b] = hex2rgb(hex);
-  const [h] = rgb2hsl(r, g, b);
-  return hsl2rgb((h * 360) | 0, s, l);
+export const colorLuminance = (hex, lum = 0) => {
+  const res = hex2rgb(hex).map((n) => {
+    const c = Math.min(Math.max(0, n + n * lum), 1);
+    return ((c * 255) | 0).toString(16).padStart(2, "0");
+  });
+  return "#" + res.join("");
 };
 
-export const defaultSettings = activityColors.map((color) => {
+export const defaultSettings = activityColors.map((color = "") => {
   const [r, g, b] = hex2rgb(color);
-  const [h, s, l] = rgb2hsl(r, g, b);
-  const bgColor = hsl2rgb((h * 360) | 0, 1, 0.5);
+  const [hue, sat, lum] = rgb2hsl(r, g, b);
+  const txtColor = getTextColor(color);
   return {
-    pickerPos: { left: s, top: l },
-    bgColor: bgColor,
-    sliderPos: h,
-    color: color,
+    hue,
+    sat,
+    lum,
+    color,
+    txtColor,
   };
 });
