852eb77796f0f9df0331acdbc340516408c8c2d1
FrontRowWithJ <adebusum@tcd.ie>
2022-02-03 01:05:35 +0000

    Removed useless useEffect

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,15 +1,9 @@
-import { useEffect } from "react";
 import "../css/error.css";
 
-const Error = ({ errorMessage, errorRef }) => {
-  useEffect(() => {
-    
-  }, [errorMessage]);
-  return (
-    <div ref={errorRef} className="error-message">
-      {errorMessage}
-    </div>
-  );
-};
+const Error = ({ errorMessage, errorRef }) => (
+  <div ref={errorRef} className="error-message">
+    {errorMessage}
+  </div>
+);
 
 export default Error;
