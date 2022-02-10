51740d5e4d45c00a8f57e33c88a97d2e9f5a9dda
FrontRowWithJ <adebusum@tcd.ie>
2022-02-10 04:25:25 +0000

    Removed unecessary ui/ux element.

--- src/components/Lesson.jsx
+++ src/components/Lesson.jsx
@@ -7,7 +7,7 @@ const getTimeRange = (s) => s.match(/[0-9]+:/g).map(toNum);
 const Lesson = ({ lesson, hour, index, currDay, settings }) => {
   const bgColor = settings[lesson.activity].color;
   const textColor = settings[lesson.activity].txtColor;
-  const { module, isOnline, activity, time, classroom, activePeriods } = lesson;
+  const { module, activity, time, classroom, activePeriods } = lesson;
   const [start, end] = getTimeRange(time);
   const opacity =
     index === currDay &&
@@ -23,13 +23,9 @@ const Lesson = ({ lesson, hour, index, currDay, settings }) => {
         boxShadow: "0px 7px 13px -7px " + bgColor,
         color: textColor,
         opacity,
-        height: `${(end - start) * 6}rem`,
+        height: `${(end - start) * 8}rem`,
       }}
     >
-      <div
-        className="online-indicator"
-        style={{ background: isOnline ? "#00FF00" : "red" }}
-      ></div>
       <div className="bottom-0" style={{ backgroundColor: bgColor }}></div>
       <div className="bottom-1" style={{ backgroundColor: bgColor }}></div>
       <div
