4cdcacc41cdcf52c4ee34afa2bd2478e4721078e
FrontRowWithJ <adebusum@tcd.ie>
2022-02-10 04:40:42 +0000

    Added touch functionality and improved ux for disabled buttons

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,29 +1,37 @@
 import { useEffect, useRef } from "react";
 import "../css/menu-item.css";
-const MenuItem = ({ text, onClick, disabled, onmousedown }) => {
+
+const MenuItem = ({ text, onClick, disabled, onMouseDown }) => {
   const noop = () => {};
+  const evtHandler = onMouseDown ?? noop;
   const buttonRef = useRef(null);
   useEffect(() => {
-    if (disabled)
-      buttonRef.current.style.cssText =
-        "font-size: unset !important; cursor: not-allowed";
+    if (disabled) buttonRef.current.style.cssText = "cursor: not-allowed";
   });
-
+  const buttonStyle = disabled ? "" : "nice-button-style";
   return (
     <div
       className="menu-item"
       style={disabled ? { opacity: 0.3 } : {}}
-      onMouseDown={onmousedown ? onmousedown : noop}
+      onMouseDown={evtHandler}
+      onTouchStart={evtHandler}
     >
       <button
+        className={buttonStyle}
         ref={buttonRef}
         type="button"
         onClick={onClick ? () => !disabled && onClick() : noop}
       >
         {text}
       </button>
-      <div className="bg-div" style={{ backgroundColor: "#585ce4" }}></div>
-      <div className="bg-div" style={{ backgroundColor: "#585ce4" }}></div>
+      <div
+        className={`bg-div ${buttonStyle}`}
+        style={{ backgroundColor: "#585ce4" }}
+      ></div>
+      <div
+        className={`bg-div ${buttonStyle}`}
+        style={{ backgroundColor: "#585ce4" }}
+      ></div>
     </div>
   );
 };
