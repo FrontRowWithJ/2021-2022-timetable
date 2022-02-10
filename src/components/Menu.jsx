d04252a2b283f4d291e09330b924ced3eeeec8c8
FrontRowWithJ <adebusum@tcd.ie>
2022-02-10 04:28:00 +0000

    Made more responsive

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,19 +8,35 @@ const Menu = ({ children }) => {
   return (
     <>
       <div
-        className="kebab-container"
-        onClick={() => {
-          kebabRef.forEach(({ current }, i) => {
-            current.classList.toggle(`anim-${list[i]}-forward`);
-            current.classList.toggle(`anim-${list[i]}-backward`);
-          });
-          menuRef.current.classList.toggle("anim-menu-forward");
-          menuRef.current.classList.toggle("anim-menu-backward");
+        className="header-button-container"
+        style={{
+          overflow: "hidden",
+          position: "absolute",
+          left: "1rem",
+          top: "1rem",
         }}
       >
-        <div ref={kebabRef[0]} className="circle anim-first-backward"></div>
-        <div ref={kebabRef[1]} className="circle anim-middle-backward"></div>
-        <div ref={kebabRef[2]} className="circle anim-last-backward"></div>
+        <div
+          className="header-button"
+          style={{
+            backgroundColor: "transparent",
+            boxShadow: "none",
+            borderRadius: "0",
+            zIndex: "3",
+          }}
+          onClick={() => {
+            kebabRef.forEach(({ current }, i) => {
+              current.classList.toggle(`anim-${list[i]}-forward`);
+              current.classList.toggle(`anim-${list[i]}-backward`);
+            });
+            menuRef.current.classList.toggle("anim-menu-forward");
+            menuRef.current.classList.toggle("anim-menu-backward");
+          }}
+        >
+          <div ref={kebabRef[0]} className="circle anim-first-backward"></div>
+          <div ref={kebabRef[1]} className="circle anim-middle-backward"></div>
+          <div ref={kebabRef[2]} className="circle anim-last-backward"></div>
+        </div>
       </div>
       <div ref={menuRef} className="menu anim-menu-backward">
         <div className="menu-item-container">{children}</div>
