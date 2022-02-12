211b9a2add51ee0b713ec2561ee5ce8c5c92f30a
FrontRowWithJ <adebusum@tcd.ie>
2022-02-12 22:02:35 +0000

    Added invisible overlay so users can close the menu when by clicking/tapping outside the menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,24 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "../css/menu.css";
 
+const [SHOWN, HIDDEN] = [true, false];
 const Menu = ({ children }) => {
   const list = ["first", "middle", "last"];
   const kebabRef = list.map(() => React.createRef(null));
   const menuRef = useRef(null);
+  const [isMenuOpened, toggleMenu] = useState(false);
+  const setMenu = (shouldShow) => {
+    const [add, remove] = shouldShow
+      ? ["forward", "backward"]
+      : ["backward", "forward"];
+    kebabRef.forEach(({ current }, i) => {
+      current.classList.add(`anim-${list[i]}-${add}`);
+      current.classList.remove(`anim-${list[i]}-${remove}`);
+    });
+    menuRef.current.classList.add(`anim-menu-${add}`);
+    menuRef.current.classList.remove(`anim-menu-${remove}`);
+    toggleMenu(shouldShow);
+  };
   return (
     <>
       <div
@@ -24,14 +38,7 @@ const Menu = ({ children }) => {
             borderRadius: "0",
             zIndex: "3",
           }}
-          onClick={() => {
-            kebabRef.forEach(({ current }, i) => {
-              current.classList.toggle(`anim-${list[i]}-forward`);
-              current.classList.toggle(`anim-${list[i]}-backward`);
-            });
-            menuRef.current.classList.toggle("anim-menu-forward");
-            menuRef.current.classList.toggle("anim-menu-backward");
-          }}
+          onClick={() => (isMenuOpened ? setMenu(HIDDEN) : setMenu(SHOWN))}
         >
           <div ref={kebabRef[0]} className="circle anim-first-backward"></div>
           <div ref={kebabRef[1]} className="circle anim-middle-backward"></div>
@@ -40,6 +47,11 @@ const Menu = ({ children }) => {
       </div>
       <div ref={menuRef} className="menu anim-menu-backward">
         <div className="menu-item-container">{children}</div>
+        <div
+          className="cancel-area"
+          style={{ display: isMenuOpened ? "" : "none" }}
+          onClick={() => setMenu(HIDDEN)}
+        ></div>
       </div>
     </>
   );
