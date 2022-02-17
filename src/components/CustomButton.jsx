7b0e43a6cf4fa3aa5dc9adc3a7aa87ef51c9e0cd
FrontRowWithJ <adebusum@tcd.ie>
2022-02-17 23:07:07 +0000

    Minor refactoring.

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -2,7 +2,7 @@ import "../css/custom-button.css";
 
 const CustomButton = ({ onClick, text, id, buttonRef }) => {
   return (
-    <div className="c-button-container" id={id}>
+    <div className="button-container" id={id}>
       <button type="button" onClick={onClick} ref={buttonRef}>
         {text}
       </button>
