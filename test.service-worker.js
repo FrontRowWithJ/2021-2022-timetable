aa3db6ade0ebdbc8f44621e1fb7877125f1a5311
FrontRowWithJ <adebusum@tcd.ie>
2022-10-01 19:33:47 +0100

    Updates

--- test.service-worker.js
+++ test.service-worker.js
@@ -1,8 +1,5 @@
 /* eslint-disable no-restricted-globals */
 /// <reference lib="webworker" />
-declare const self: ServiceWorkerGlobalScope;
-
-export { }
 
 self.addEventListener("push", (event) => {
   if (event.data) {
@@ -10,7 +7,10 @@ self.addEventListener("push", (event) => {
     let title = "";
     if (payload.type === "registration") title = "Registration Successful";
     if (payload.type === "college") title = "Time for Class!";
-    const options = { body: payload.message, icon: "android-chrome-192x192.png", };
-    title && self.registration.showNotification(title, options)
+    const options = {
+      body: payload.message,
+      icon: "android-chrome-192x192.png",
+    };
+    title && self.registration.showNotification(title, options);
   }
 });
